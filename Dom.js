35195ace8e2c7667a84d5709277051b0db506fd9
Nuclear One <ajithsnair10@gmail.com>
2022-02-17 11:53:22 +0530

    Dom manipulation first phace

---
+++ Dom.js
@@ -0,0 +1,44 @@
+// let ele=document.querySelector('#my-form')
+// console.log(ele.parentNode)
+// ele.parentNode.style.backgroundColor='gray'
+// console.log(ele.parentNode.parentNode)
+
+//paraentelement
+// let ele=document.querySelector('#my-form')
+// console.log(ele.parentElement)
+// ele.parentElement.style.backgroundColor='gray'
+// console.log(ele.parentElement.parentElement)
+
+//childnode
+ //let ele=document.querySelector('#container')
+// console.log(ele.childNodes)
+ //let el=document.querySelector('#my-form')
+// console.log(el.childNodes)
+// let ms=document.querySelector('#itemss')
+// console.log(ms.children)
+// ms.children[2].style.backgroundColor='yellow';
+
+//firstChild
+// console.log(ms.firstChild)
+
+//FirstElement
+// console.log(ms.firstElementChild)
+// ms.firstElementChild.textContent='your hacked';
+
+//lastElement
+// ms.lastElementChild.textContent='your hacked too';
+
+//sibling
+// console.log(ms.nextSibling)
+
+//nextelementsibling
+// console.log(ms.nextElementSibling)
+
+//previoussibling
+// console.log(ms.previousSibling)
+
+//previouselementsibiling
+// console.log(ms.previousElementSibling)
+// ms.previousElementSibling.previousElementSibling.style.backgroundColor='green';
+
+
